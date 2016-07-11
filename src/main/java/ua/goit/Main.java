cb194fb093f62133c567f72447a0c33209384dfc
4594400 <4594400@gmail.com>
2016-07-11 16:45:17 +0300

    Initial commit

--- src/main/java/ua/goit/Main.java
+++ src/main/java/ua/goit/Main.java
@@ -5,30 +5,26 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import ua.goit.common.OperationExecutor;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
     public static void main(String[] args) {
 
         ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
-
-        System.out.println(getOperationExecutor(applicationContext).executeOperation("32 - 5"));
-        System.out.println(getOperationExecutor(applicationContext).executeOperation("800 - 200"));
-        System.out.println(getOperationExecutor(applicationContext).executeOperation("32.6f - 6.3f"));
-        System.out.println(getOperationExecutor(applicationContext).executeOperation("52.6 - 16.3"));
-
-
-
-       /* ApplicationContext applicationContext = new ClassPathXmlApplicationContext("application-context.xml");
-
         OperationExecutor operationExecutor = applicationContext.getBean("operationExecutor", OperationExecutor.class);
-        System.out.println(operationExecutor.executeOperation("32 - 5"));
-        System.out.println(operationExecutor.executeOperation("800 - 200"));
-        System.out.println(operationExecutor.executeOperation("32.6f - 6.3f"));
-        System.out.println(operationExecutor.executeOperation("52.6 - 16.3"));*/
 
 
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Please, input expression. For example: 5 + 3 or 95 - 5.");
+        try {
+            String expression = reader.readLine();
+            System.out.println(expression + " = " + operationExecutor.executeOperation(expression));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    private static OperationExecutor getOperationExecutor(ApplicationContext applicationContext) {
-        return applicationContext.getBean("operationExecutor", OperationExecutor.class);
-    }
+
 }
